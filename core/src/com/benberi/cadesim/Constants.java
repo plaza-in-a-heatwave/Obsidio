82a1ccf386432b7e310b57a5bee06b324a8f396b
alex <alex@alex>
2020-08-18 01:24:17 +0100

    bump version to 1.9.92

--- core/src/com/benberi/cadesim/Constants.java
+++ core/src/com/benberi/cadesim/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
     /**
      * Version of client
      */
-    public static final String VERSION = "1.9.91";
+    public static final String VERSION = "1.9.92";
     public static final int PROTOCOL_VERSION = 12; // MUST match server
 
 	public static final int MAX_NAME_SIZE = 19;   // name of player
